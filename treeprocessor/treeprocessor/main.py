19abb574506b6c5fd69db9e7163ef6807756851e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2024-04-25 09:42:35 -0400

    chore: Add a TODO comment reminder in the treeprocessor/treeprocessor/main.py.

--- treeprocessor/treeprocessor/main.py
+++ treeprocessor/treeprocessor/main.py
@@ -14,29 +14,25 @@ cli = typer.Typer()
 # create a console for display of rich text
 console = Console()
 
+# TODO: Add comments to explain the steps in the main function
+
 
 @cli.command()
 def main(directory: Path = typer.Option(...)):
     """Traverse a file system tree."""
-    # display details about the configuration of the traversal tool
     console.print("\n[bold red]File system traversal tool[/bold red]\n")
     console.print(f"Chosen directory: {directory}")
-    # traverse the filesystem tree
     (filesystem_tree, filesystem_tree_sizes) = build_directory_tree(
         directory, None, {}
     )
-    # display the filesystem tree
     console.print("\n[bold red]File system tree hierarchy[/bold red]\n")
     console.print(filesystem_tree)
-    # display the sizes of the files in the filesystem tree
     console.print("\n[bold red]File system tree sizes (in bytes)[/bold red]\n")
     for path, size in filesystem_tree_sizes.items():
         console.print(f"{path}: {size} bytes")
-    # calculate statistics about the sizes of the files in the directory
     min_size, min_files, max_size, max_files, avg_size = calculate_file_stats(
         filesystem_tree_sizes
     )
-    # display the statistics about the sizes of the files in the directory
     console.print("\n[bold red]File system tree statistics[/bold red]\n")
     console.print(f"Minimum file size: {min_size} bytes")
     console.print("Files with minimum size:")
